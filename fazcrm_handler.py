2795a86fae155aebb8c9231cbce27881f894570f
Fernando Augusto <fams@linuxplace.com.br>
2023-02-22 19:37:09 -0300

    Adding logging and a strip

--- fazcrm_handler.py
+++ fazcrm_handler.py
@@ -1,11 +1,15 @@
 import sys
 from sources.explorer import Explorer
+import logging
+
+logging.basicConfig(filename="C:\\src\\fazcrm_handler\\log.txt",
+                    level=logging.ERROR)
 
 GOOGLE_DRIVE_PATH = r"G:\Meu Drive"
 
 e = Explorer(GOOGLE_DRIVE_PATH)
-
 if len(sys.argv) > 1:
     url = sys.argv[1]
-    relative_path = url.replace("fazcrm://", "")
+    relative_path = url.replace("fazcrm://", "").rstrip("/")
+    logging.debug("Open File {}".format(relative_path))
     e.open(relative_path)
